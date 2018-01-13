83526dd86b6db1867572d8872ecf7c81060f55f0
Benjamin Gill <ben_gill@yahoo.com>
2018-01-13 12:23:45 -0500

    adding ignorable words

--- src/parser.js
+++ src/parser.js
@@ -1,11 +1,30 @@
-import find from 'lodash/find';
+import _ from 'lodash';
 import commands from './commands';
 
+const wordsToIgnore = ['the', 'at', 'a'];
+
 const isMatch = (command, input) => input.match(command.pattern);
-const findCommand = input => find(commands, command => isMatch(command, input));
+const findCommand = input => _.find(commands, command => isMatch(command, input));
 
 export default function (container, input) {
-  const command = findCommand(input);
+  const tokens = _.split(input, ' ');
+
+  let i = 1;
+  while (i < tokens.length - 1) {
+    if (_.includes(wordsToIgnore, tokens[i])) {
+      const beforeTokenIsValid = !_.includes(wordsToIgnore, tokens[i - 1]);
+      const afterTokenIsValid = !_.includes(wordsToIgnore, tokens[i + 1]);
+      if (beforeTokenIsValid && afterTokenIsValid) {
+        tokens.splice(i, 1);
+      } else {
+        i += 1;
+      }
+    } else {
+      i += 1;
+    }
+  }
+
+  const command = findCommand(_.join(tokens, ' '));
   if (command) {
     command.action(container, input.match(command.pattern));
     return true;
